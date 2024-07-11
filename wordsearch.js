a9119dd0cf63d651e21da6489aa404de506fc1d7
Henry <mbahenryemeka@gmail.com>
2024-07-11 15:23:11 -0400

    wordSearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,15 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {     
+    const transpose = (matrix) => {
+        return matrix[0].map((_, colIndex) => matrix.map(row => row[colIndex]));
+    };
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    for (const l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+    return false;
 }
-
 module.exports = wordSearch
